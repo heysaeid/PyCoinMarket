0cc1af63f855917922b6d3fae903db7d5a392172
heysaeid <heysaeid92@gmail.com>
2024-08-10 23:49:33 +0330

    Add BaseCryptoAPI

---
+++ py_coin_market/base.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+
+class BaseCryptoAPI(ABC):
+    ...
+
+    @abstractmethod
+    def get_category_list(self): 
+        pass
+
+    @abstractmethod
+    def get_category_info(self): 
+        pass
+
+    @abstractmethod
+    def get_cryptocurrency_list(self): 
+        pass
+
+    @abstractmethod
+    def get_cryptocurrency_info(self): 
+        pass
